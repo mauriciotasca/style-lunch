8dc97a2f254fb9c3526bbc4f1b70c70d1c841b44
Mauricio Correa <mauricio.correa@macys.com>
2019-07-16 03:00:26 -0300

    Add Prop Types and shape prop

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './Box.scss';
+import PropTypes from 'prop-types';
 
 class Box extends Component {
+    static propTypes = {
+        /** Will determine which color the box background is going to be */
+        color: PropTypes.string,
+        /** Will determine which shape the box is going to represent */
+        shape: PropTypes.string
+    };
+    static defaultProps = {
+        color: 'gray',
+        shape: 'square'
+    };
+
     render() {
-        const { color } = this.props;
+        const { color, shape } = this.props;
 
         return (
-            <div className={`box ${color ? color : 'gray'}`}>
+            <div className={`box ${color} ${shape}`}>
                 <h3>I'm a box</h3>
             </div>
         );
